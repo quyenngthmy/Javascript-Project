a93d5c3af8733b7745b84ae3d80a481c7e6a442c
TMT\quyenntm <quyen.ngthmy@gmail.com>
2023-01-18 14:05:22 +0700

    update

--- src/js/oop.js
+++ src/js/oop.js
@@ -9,13 +9,20 @@ const playerImage = new Image();
 import shadowDog from '../img/oop/shadow-dog.png';
 playerImage.src = shadowDog;
 
-const spriteWidth = 
+const spriteWidth = 575; // 6876(width Image)/12 = 573px
+const spriteHeight = 523; // 5230(height Image)/10 = 523;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillRect(50, 50, 100, 100);
+    let position = Math.floor(gameFrame/staggerFrames) % 6;
+    frameX = spriteWidth * position;
     // ctx.drawImage(Image, sx, sy, sw, sh, dx, dy, dw, dh);
-    ctx.drawImage(playerImage, 0, 0);
+    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    gameFrame++;
     requestAnimationFrame(animate);
 };
 
